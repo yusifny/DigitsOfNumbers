f1fc14a521f9c79984fd6226f125568dfb808499
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-06-11 22:34:05 +0400

    uploaded second time

---
+++ DigitsOfNumbers/DigitsOfNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static  System.Console;
+
+namespace DigitsOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter a number: ");
+            int number = Convert.ToInt32(ReadLine());
+            int numberB = 0;
+            while (number>0)
+            {
+                number /= 10;
+                numberB++;
+            }
+
+            WriteLine($"The entered number has {numberB} digit(s).");
+        }
+    }
+}
